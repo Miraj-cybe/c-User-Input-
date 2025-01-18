3b5ea79a4e1e62e0e3692b097ba9482c7ee5376a
Miraj-cybe <mondolm619@gmail.com>
2025-01-18 14:47:54 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class UserInput
+{
+public static void Main(string[] args)
+{
+    Console.Write("Enter user Name: ");
+   string? studentName =Console.ReadLine();
+   Console.WriteLine("welcome vr mode "+ studentName);
+
+int studentAge;
+Console.Write("Enter Your Age: ");
+studentAge=Convert.ToInt32(Console.ReadLine());
+Console.Write( "Age: "+ studentAge +" Years Old");
+
+
+
+
+}
+
+}
